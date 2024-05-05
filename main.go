ed8ba85b919fbc02e1c55c11cf41898eb9424f37
Diogo Gaspar <diogo.gaspar@me.com>
2024-05-04 21:07:57 -0300

    refac: Codeium changed the code better than Gemini and GPT
    I asked for clean code and Solid and it did a great job imo

--- main.go
+++ main.go
@@ -5,12 +5,36 @@ import (
 	"fmt"
 	"log"
 	"os"
-
-	_ "github.com/lib/pq"
 )
 
-var db *sql.DB
+// Config contains all the necessary data to establish a connection to the database.
+type Config struct {
+	User     string
+	Password string
+	Host     string
+	Port     int
+	Database string
+}
 
+// Database is a struct that holds the established connection to the database.
+type Database struct {
+	conn *sql.DB
+}
+
+// AlbumsRepository is an interface that defines the methods needed to interact with the database.
+type AlbumsRepository interface {
+	GetByArtist(name string) ([]Album, error)
+	GetById(id int64) (Album, error)
+	Add(album Album) (int64, error)
+	Delete(id int64) error
+}
+
+// PsqlAlbumsRepository is a struct that implements the AlbumsRepository interface and contains a reference to the database.
+type PsqlAlbumsRepository struct {
+	db *Database
+}
+
+// Album is a struct that represents an album in the database.
 type Album struct {
 	ID     int64
 	Title  string
@@ -18,112 +42,80 @@ type Album struct {
 	Price  float32
 }
 
-func main() {
-	cfg := struct {
-		User     string
-		Password string
-		Host     string
-		Port     int
-		Database string
-	}{
-		User:     os.Getenv("DBUSER"),
-		Password: os.Getenv("DBPASS"),
-		Host:     "localhost",
-		Port:     5432,
-		Database: "recordings",
-	}
-
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-		cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Database)
-
-	var err error
-	db, err = sql.Open("postgres", psqlInfo)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if err = db.Ping(); err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Connected to Postgres!")
-
-	albums, err := albumsByArtist("John Coltrane")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Albums found:", albums)
-
-	album, err := albumsByID(3)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println("Album found:", album)
-
-	albumID, err := addAlbum(Album{
-		Title:  "The Modern Sound of Betty Carter",
-		Artist: "Betty Carter",
-		Price:  49.99,
-	})
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Printf("ID of added album: %v\n", albumID)
-
-	// if err = removeAlbum(9); err != nil {
-	// 	log.Fatal(err)
-	// }
+// GetByArtist retrieves albums from the database based on the artist name provided.
+func (r *PsqlAlbumsRepository) GetByArtist(name string) ([]Album, error) {
+	// Query to retrieve albums based on the artist name.
+	query := "SELECT * FROM album WHERE artist = $1"
+	return r.fetchAlbums(query, name)
 }
 
-// get all albums for an artist
-func albumsByArtist(name string) ([]Album, error) {
-	var albums []Album
+// GetById retrieves an album from the database based on its ID.
+func (r *PsqlAlbumsRepository) GetById(id int64) (Album, error) {
+	// Query to retrieve an album based on its ID.
+	query := "SELECT * FROM album WHERE id = $1"
+	return r.fetchAlbum(query, id)
+}
 
-	rows, err := db.Query("SELECT * FROM album WHERE artist = $1", name)
+// Add inserts a new album into the database and returns its ID.
+func (r *PsqlAlbumsRepository) Add(album Album) (int64, error) {
+	// Query to insert a new album into the database.
+	query := "INSERT INTO album (title, artist, price) VALUES ($1, $2, $3) RETURNING id"
+	return r.insertAlbum(query, album)
+}
+
+// Delete deletes an album from the database based on its ID.
+func (r *PsqlAlbumsRepository) Delete(id int64) error {
+	// Query to delete an album from the database.
+	query := "DELETE FROM album WHERE id = $1"
+	_, err := r.db.conn.Exec(query, id)
 	if err != nil {
-		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+		return fmt.Errorf("removeAlbum: %v", err)
 	}
 
-	// try to move this defer to the bottom of the function, before the return
+	return nil
+}
+
+// fetchAlbums retrieves multiple albums from the database based on a query and name.
+func (r *PsqlAlbumsRepository) fetchAlbums(query, name string) ([]Album, error) {
+	rows, err := r.db.conn.Query(query, name)
+	if err != nil {
+		return nil, err
+	}
 	defer rows.Close()
+
+	var albums []Album
 	for rows.Next() {
 		var album Album
-		if err := rows.Scan(&album.ID, &album.Title, &album.Artist, &album.Price); err != nil {
-			return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+		err := rows.Scan(&album.ID, &album.Title, &album.Artist, &album.Price)
+		if err != nil {
+			return nil, err
 		}
 		albums = append(albums, album)
 	}
 
-	if err := rows.Err(); err != nil {
-		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
-	}
-
-	return albums, nil
+	return albums, rows.Err()
 }
 
-// get one album by id
-func albumsByID(id int64) (Album, error) {
+// fetchAlbum retrieves a single album from the database based on a query and ID.
+func (r *PsqlAlbumsRepository) fetchAlbum(query string, id int64) (Album, error) {
+	row := r.db.conn.QueryRow(query, id)
 	var album Album
-
-	row := db.QueryRow("SELECT * FROM album WHERE id = $1", id)
-
-	if err := row.Scan(&album.ID, &album.Title, &album.Artist, &album.Price); err != nil {
-		if err == sql.ErrNoRows {
-			return album, fmt.Errorf("albumsById %d: no such album", id)
-		}
-		return album, fmt.Errorf("albumsById %d: %v", id, err)
+	err := row.Scan(&album.ID, &album.Title, &album.Artist, &album.Price)
+	if err == sql.ErrNoRows {
+		return album, fmt.Errorf("albumsById %d: no such album", id)
 	}
+
+	if err != nil {
+		return album, err
+	}
+
 	return album, nil
 }
 
-func addAlbum(album Album) (int64, error) {
+// insertAlbum inserts a new album into the database and returns its ID.
+func (r *PsqlAlbumsRepository) insertAlbum(query string, album Album) (int64, error) {
 	var id int64
-	err := db.QueryRow(
-		"INSERT INTO album (title, artist, price) VALUES ($1, $2, $3) RETURNING id", album.Title, album.Artist, album.Price).Scan(&id)
-
+	err := r.db.conn.QueryRow(query, album.Title, album.Artist, album.Price).Scan(&id)
 	if err != nil {
 		return 0, fmt.Errorf("addAlbum: %v", err)
 	}
@@ -131,12 +123,69 @@ func addAlbum(album Album) (int64, error) {
 	return id, nil
 }
 
-func removeAlbum(id int64) error {
-	_, err := db.Exec("DELETE FROM album WHERE id = $1", id)
+// main is the entry point of the program. It establishes a connection to the database and performs some operations.
+func main() {
+	// Configuration for the database.
+	config := Config{
+		User:     os.Getenv("DBUSER"),
+		Password: os.Getenv("DBPASS"),
+		Host:     "localhost",
+		Port:     5432,
+		Database: "recordings",
+	}
+	// Establish a connection to the database.
+	db, err := ConnectToDB(config)
 	if err != nil {
-		return fmt.Errorf("removeAlbum: %v", err)
+		log.Fatal(err)
 	}
 
-	fmt.Printf("Album %d deleted\n", id)
-	return nil
+	// Create a new PsqlAlbumsRepository instance with the established connection.
+	repo := PsqlAlbumsRepository{db: db}
+
+	// Retrieve albums by artist.
+	albums, err := repo.GetByArtist("John Coltrane")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Albums found:", albums)
+
+	// Retrieve an album by its ID.
+	album, err := repo.GetById(3)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Album found:", album)
+
+	// Add a new album to the database and retrieve its ID.
+	albumID, err := repo.Add(Album{
+		Title:  "The Modern Sound of Betty Carter",
+		Artist: "Betty Carter",
+		Price:  49.99,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("ID of added album: %v\n", albumID)
+}
+
+// ConnectToDB establishes a connection to the database using the provided configuration.
+func ConnectToDB(config Config) (*Database, error) {
+	connStr := psqlInfo(config)
+	conn, err := sql.Open("postgres", connStr)
+	if err != nil {
+		return nil, err
+	}
+
+	err = conn.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return &Database{conn: conn}, nil
+}
+
+// psqlInfo generates a connection string for the database based on the provided configuration.
+func psqlInfo(config Config) string {
+	return fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		config.Host, config.Port, config.User, config.Password, config.Database)
 }
