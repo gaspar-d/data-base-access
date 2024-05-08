219c7a67b7af9a09660401755f0e0d0db8ff0c60
Diogo Gaspar <diogo.gaspar@me.com>
2024-05-07 23:59:37 -0300

    chore: create two files for better readability
    it looks like it the package name is the same i don't need to
    import the file into main.go
    this makes this similar to swift, bu i need to see what happens
    when we compile it

--- main.go
+++ main.go
@@ -12,13 +12,6 @@ import (
 
 var db *sql.DB
 
-type Album struct {
-	ID     int64
-	Title  string
-	Artist string
-	Price  float32
-}
-
 func main() {
 	cfg := struct {
 		User     string
@@ -77,67 +70,4 @@ func main() {
 	// if err = removeAlbum(5); err != nil {
 	// 	log.Fatal(err)
 	// }
-}
-
-// get all albums for an artist
-func albumsByArtist(name string) ([]Album, error) {
-	var albums []Album
-
-	rows, err := db.Query("SELECT * FROM album WHERE artist = $1", name)
-	if err != nil {
-		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
-	}
-
-	// try to move this defer to the bottom of the function, before the return
-	defer rows.Close()
-	for rows.Next() {
-		var album Album
-		if err := rows.Scan(&album.ID, &album.Title, &album.Artist, &album.Price); err != nil {
-			return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
-		}
-		albums = append(albums, album)
-	}
-
-	if err := rows.Err(); err != nil {
-		return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
-	}
-
-	return albums, nil
-}
-
-// get one album by id
-func albumsByID(id int64) (Album, error) {
-	var album Album
-
-	row := db.QueryRow("SELECT * FROM album WHERE id = $1", id)
-
-	if err := row.Scan(&album.ID, &album.Title, &album.Artist, &album.Price); err != nil {
-		if err == sql.ErrNoRows {
-			return album, fmt.Errorf("albumsById %d: no such album", id)
-		}
-		return album, fmt.Errorf("albumsById %d: %v", id, err)
-	}
-	return album, nil
-}
-
-func addAlbum(album Album) (int64, error) {
-	var id int64
-	err := db.QueryRow(
-		"INSERT INTO album (title, artist, price) VALUES ($1, $2, $3) RETURNING id", album.Title, album.Artist, album.Price).Scan(&id)
-
-	if err != nil {
-		return 0, fmt.Errorf("addAlbum: %v", err)
-	}
-
-	return id, nil
-}
-
-func removeAlbum(id int64) error {
-	_, err := db.Exec("DELETE FROM album WHERE id = $1", id)
-	if err != nil {
-		return fmt.Errorf("removeAlbum: %v", err)
-	}
-
-	fmt.Printf("Album %d deleted\n", id)
-	return nil
 }
